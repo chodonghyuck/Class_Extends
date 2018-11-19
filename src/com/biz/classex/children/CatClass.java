5738b45684c740ab91b558ab1c7ce86fbec0429a
chodonghyuck <colplayer@naver.com>
2018-11-19 16:27:17 +0900

    first

---
+++ src/com/biz/classex/children/CatClass.java
@@ -0,0 +1,16 @@
+package com.biz.classex.children;
+
+import com.biz.classex.parents.AnimalClass;
+
+public class CatClass extends AnimalClass {
+
+	// @Override (재정의)
+	
+	
+	public void whatIsName() {
+		System.out.println("나는 페르시안 입니다.");
+	}
+	
+	
+
+}
